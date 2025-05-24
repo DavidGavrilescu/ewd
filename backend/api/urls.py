c5d1767a4b980c59bbd58ebf043afb83c72c6090
David Gavrilescu <davidgavrilescu@MacBook-Air-David.local>
2025-05-24 19:12:05 +0300

    update general format

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('postari-blog/', views.lista_postari_blog, name='lista_postari_blog'),
-    path('postari-blog/<int:id>/', views.detaliu_postare_blog, name='detaliu_postare_blog'),  # <-- ADĂUGAT
+    path('postari-blog/<int:id>/', views.detaliu_postare_blog, name='detaliu_postare_blog'),
     path('mesaje-contact/', views.lista_mesaje_contact, name='lista_mesaje_contact'),
     path('elemente-galerie/', views.lista_elemente_galerie, name='lista_elemente_galerie'),
     path('intrebari-bot/', views.lista_intrebari_bot, name='lista_intrebari_bot'),
